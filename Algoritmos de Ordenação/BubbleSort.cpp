91f175ed5c059e956989e276d184e13d012b9884
root <root@GustavoNotebook>
2019-06-11 21:52:22 -0300

    Método Simples

--- Algoritmos de Ordenação/BubbleSort.cpp
+++ Algoritmos de Ordenação/BubbleSort.cpp
@@ -4,11 +4,11 @@ Pega o número e o próximo do vetor, colocando os dois em ordem,
 fazendo vários loops até que todo o vetor fique ordenado.
 */
 
-int continua, aux, fin = N;
+int continua, fim = n, aux;
 
 do {
     continua = 0;
-    for (int i = 0; < fim -1; i++){
+    for (int i = 0; i < fim -1; i++){
         if (vetor[i] > vetor[i+1]) {
             aux = vetor[i];
             vetor[i] = vetor[i+1];
