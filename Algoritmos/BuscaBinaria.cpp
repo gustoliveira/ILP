6326188c278738f6742a148395a93e19d46dd476
gustoliveira <gust.oliveira.ferreira@gmail.com>
2019-09-21 06:53:57 -0300

    Consertando BuscaBinaria

--- Algoritmos/BuscaBinaria.cpp
+++ Algoritmos/BuscaBinaria.cpp
@@ -1,18 +1,10 @@
 int buscaBinaria(int vet[], int n, int chave) {
      int posIni = 0, posFim = n - 1, posMeio;
-     int flag = -1;
      while (posIni <= posFim) {
-          posMeio = (posIni + posFim) / 2;
-          if (vet[posMeio] == chave){
-               flag = posMeio;
-               break;
-          }
-          else if (vet[posMeio] > chave){
-               posFim = posMeio - 1;
-          }
-          else if(vet[posMeio] < chave){
-               posIni = posMeio + 1;
-          }               
+          posMeio = (posIni + posFim)/2;
+          if (vet[posMeio] == chave) return posMeio;
+          else if (vet[posMeio] > chave) posFim = posMeio - 1;
+          else if (vet[posMeio] < chave) posIni = posMeio + 1;
      }
-     return flag;
+     return -1;
 }
