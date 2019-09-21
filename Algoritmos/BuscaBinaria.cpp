2afe147aa6c7367d048b432fa86ba933659d3eb5
gustoliveira <gust.oliveira.ferreira@gmail.com>
2019-09-21 06:48:46 -0300

    BuscaBinaria.cpp

--- Algoritmos/BuscaBinaria.cpp
+++ Algoritmos/BuscaBinaria.cpp
@@ -1,18 +1,18 @@
-//ESTÁ ERRADO
-
-int pesquisaBinaria (int vet[], int chave, int tam){
-     int inf = 0;     // limite inferior (o primeiro índice de vetor em C é zero)
-     int sup = tam-1; // limite superior (termina em um número a menos. 0 a 9 são 10 números)
-     int meio;
-     while (inf <= sup)
-     {
-          meio = (inf + sup)/2;
-          if (chave == vet[meio])
-               return meio;
-          if (chave < vet[meio])
-               sup = meio-1;
-          else
-               inf = meio+1;
+int buscaBinaria(int vet[], int n, int chave) {
+     int posIni = 0, posFim = n - 1, posMeio;
+     int flag = -1;
+     while (posIni <= posFim) {
+          posMeio = (posIni + posFim) / 2;
+          if (vet[posMeio] == chave){
+               flag = posMeio;
+               break;
+          }
+          else if (vet[posMeio] > chave){
+               posFim = posMeio - 1;
+          }
+          else if(vet[posMeio] < chave){
+               posIni = posMeio + 1;
+          }               
      }
-     return -1; //Se achou, retorna a posição, se não, a função retorna -1
+     return flag;
 }
