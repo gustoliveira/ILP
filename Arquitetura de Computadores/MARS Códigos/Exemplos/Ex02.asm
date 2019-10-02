8bc20054d15a40a4ff5a97f0f220ac84ba4570db
gustoliveira <gust.oliveira.ferreira@gmail.com>
2019-10-02 17:40:17 -0300

    Mudando exemplos

--- Arquitetura de Computadores/MARS Códigos/Exemplos/Ex02.asm
+++ Arquitetura de Computadores/MARS Códigos/Exemplos/Ex02.asm
@@ -5,12 +5,12 @@
 	A: .float 40.0		# declaracao de variavel float
 	B: .float 30.0
 
-.text   
-.globl  main            	
-        
+.text
+.globl  main
+
 main:   l.s $f0, A		# carrega valor float para $f0
 	l.s $f1, B		# carrega valor float para $f1
 	add.s $f2, $f0, $f1	# soma de valores float, com resultado em $f2
-	
+
 	li $v0,10		# chamada de sistema para encerrar o programa (código 10 em $v0)
 	syscall
