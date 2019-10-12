96551383611afed45cbe7f08d1c20cb34220dcfb
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2019-10-12 01:14:49 -0300

    "Lista 4: map"

--- LAB1/UFBACard.cpp
+++ LAB1/UFBACard.cpp
@@ -1,41 +1,43 @@
-//ERRADA
-
+//Ainda errada
 #include <iostream>
 #include <map>
+#include <cmath>
 using namespace std;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+    int qntAlunos, qntAulas;
+    cin >> qntAlunos >> qntAulas;
 
-    map<int, float> aluno;
-    map<int, float>:: iterator it;
+    map<int, float> alunos;
+    map<int, float>::iterator it;
 
-    for(int i = 0; i < n; i++){
-        int aux1;
-        cin >> aux1;
-        aluno[aux1] = 0;
+    for(int i = 0; i < qntAlunos; i++){
+        int aux;
+        cin >> aux;
+        alunos[aux] = 0.0;
     }
 
-    for(int i = 0; i < m; i++){
-        int aux1;
-        float aux2;
-        cin >> aux1;
-        for(int j = 0; j < aux1; j++){
-            cin >> aux2;
-            aluno[aux1]++;
+    for(int i = 0; i < qntAulas; i++){
+        int qntPresentes, aux;
+        cin >> qntPresentes;
+        for(int j = 0; j < qntPresentes; j++){
+            cin >> aux;
+            alunos[aux] += 1.0;
         }
     }
 
-    float faltas = m*(0,25);
+    float maxFaltas = ceil(qntAulas*0.25); //Quantidade maxima que se pode faltar
+    cout << maxFaltas << endl;
 
-    for(it = aluno.begin(); it != aluno.end(); it++){
-        if(it->second <= faltas) {
-            cout << it->first << ": " << "RF" << endl;
+    for(it = alunos.begin(); it != alunos.end(); it++){
+        float aux = it->second; //Quantas aulas o aluno efetivamente foi
+
+        if(aux > maxFaltas) {
+            cout << it->first << ": " << qntAulas-aux << endl;
+            continue;
+
         }
-        else {
-            cout << it->first << ": " << (it->second)-m << endl;
-        }
+        cout << it->first << ": RF" << endl;
     }
 
     return 0;
