ee877954f2fe96bdd7ab1892dbe2b5980408df95
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2019-11-09 11:25:26 -0300

    Lista 4: map

--- LAB1/UFBACard.cpp
+++ LAB1/UFBACard.cpp
@@ -8,13 +8,13 @@ int main(){
     int qntAlunos, qntAulas;
     cin >> qntAlunos >> qntAulas;
 
-    map<int, float> alunos;
-    map<int, float>::iterator it;
+    map<int, int> alunos;
+    // map<int, float>::iterator it;
 
     for(int i = 0; i < qntAlunos; i++){
         int aux;
         cin >> aux;
-        alunos[aux] = 0.0;
+        alunos[aux] = 0;
     }
 
     for(int i = 0; i < qntAulas; i++){
@@ -22,22 +22,17 @@ int main(){
         cin >> qntPresentes;
         for(int j = 0; j < qntPresentes; j++){
             cin >> aux;
-            alunos[aux] += 1.0;
+            alunos[aux]++;
         }
     }
 
-    float maxFaltas = ceil(qntAulas*0.25); //Quantidade maxima que se pode faltar
-    cout << maxFaltas << endl;
-
-    for(it = alunos.begin(); it != alunos.end(); it++){
-        float aux = it->second; //Quantas aulas o aluno efetivamente foi
-
-        if(aux > maxFaltas) {
-            cout << it->first << ": " << qntAulas-aux << endl;
-            continue;
-
-        }
-        cout << it->first << ": RF" << endl;
+    for(pair<int, int> p:alunos){
+        int matricula = p.first, presencas = p.second; //Quantas aulas o aluno efetivamente foi
+        cout << matricula << ": ";
+        if(presencas < 0.75*qntAulas)
+            cout << "RF" << endl;
+        else
+            cout << qntAulas - presencas << endl;
     }
 
     return 0;
